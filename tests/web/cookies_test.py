3d3e0d264998f87dde68526230c46b5e32664d3a
Stacy Pavlenko <tena.pavlenko@gmail.com>
2026-03-06 23:30:38 +0200

    MF-8-1 Fixed linted detected errors

--- tests/web/cookies_test.py
+++ tests/web/cookies_test.py
@@ -1,7 +1,10 @@
+import pytest
+
 from src.web.application import Application
 from tests.fixtures.cookie_helper import CookieHelper
 
 
+@pytest.mark.regression
 def test_add_feature_flag_cookie(logged_app: Application, cookies: CookieHelper):
     cookies.add("feature_flag", "dark_mode_enabled", "app.testomat.io")
 
