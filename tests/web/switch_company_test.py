c64cb565f83ae25350a1944ecbcd151b00836d08
Stacy Pavlenko <tena.pavlenko@gmail.com>
2026-03-06 23:43:39 +0200

    MF-8-1 Fixed linted detected errors

--- tests/web/switch_company_test.py
+++ tests/web/switch_company_test.py
@@ -1,9 +1,11 @@
+import pytest
 from playwright.sync_api import expect
 
 from src.web.application import Application
 from tests.fixtures.infrastructure_handler import DEFAULT_COMPANY
 
 
+@pytest.mark.smoke
 def test_project_page_header(remove_test_projects, logged_app: Application):
     proj_page = logged_app.projects_page.is_loaded().open_company_projects(DEFAULT_COMPANY)
 
